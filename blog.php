7f1c0f7d25c9345c1d9b42f24fb9f7bccb05494f
shubhamskq <79262215+shubhamskq@users.noreply.github.com>
2023-08-13 22:27:00 +0530

    Create blog.php

---
+++ blog.php
@@ -0,0 +1,17 @@
+<?php 
+include_once "include/header.php";
+?>
+
+<div class="container-fluid">
+    <h5 class="mb- text-grey-800">Blog Posts</h5>
+    <div class="card shadow">
+        <div class="card-header py- d-flex justigy-content-between">
+            <div>
+                <a href="addblog.php">
+                    <h6>
+                    add new</h6>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
